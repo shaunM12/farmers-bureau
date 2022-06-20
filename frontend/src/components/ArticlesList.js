a0333eb4f44921e204a55178213ded7052fe1584
shaunM12 <92561464+shaunM12@users.noreply.github.com>
2022-06-20 09:44:54 -1000

    node modules

--- frontend/src/components/ArticlesList.js
+++ frontend/src/components/ArticlesList.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { getArticles } from "../functions";
-import '../styles/ArticlesList.style.css'
-import {
-  ArticlesListContainer
-} from '../styles/ArticlesList.style'
+// import '../styles/ArticlesList.style.css'
+// import {
+//   ArticlesListContainer
+// } from '../styles/ArticlesList.style'
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
